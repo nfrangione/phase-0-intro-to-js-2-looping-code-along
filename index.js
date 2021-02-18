62408787ed372e3839d06e129745b4c5c7664d9b
Nicholas V Frangione <nfrangione8@gmail.com>
2021-02-18 15:29:25 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, reason) {
+    let thankYouCards = []
+    for (let i = 0; i < names.length; i++) {
+      thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${reason} gift!`);
+    }
+    return thankYouCards;
+  }
+
+function countDown(count) {
+    while ( count > 0) {
+        console.log(count);
+        count -= 1;
+    }
+    console.log(count);
+}
